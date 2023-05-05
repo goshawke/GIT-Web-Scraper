0ee01cc72b57d903c74d783e3e7094d7ee5740a9
Luke Ross <lukasross19@gmail.com>
2023-05-05 16:21:23 -0400

    remove auto-deleted val

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ def home():
 def searchResults(term):
     print("search term is: " + term)
 
+    args = request.args
     lang = args["lang"]
 
     url = f'https://github.com/search?l={lang}&o=desc&s=updated&type=Repositories&q={term}&p=1'
